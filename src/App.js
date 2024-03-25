0f360f838bb28ff5e8e7e0e0eb553171b58eab81
Depanshu <depanshusahu057@gmail.com>
2024-03-25 22:32:21 +0530

    added 2nd and data page

--- src/App.js
+++ src/App.js
@@ -1,89 +1,186 @@
 import Spline from "@splinetool/react-spline";
 import Navbar from "./Navbar";
-import "./app.css"
+import "./app.css";
+import Grid from "./Grid";
+import data from "./data.json";
+import { useState } from "react";
 
 function App() {
+  const [result, setResult] = useState(data.slice(0, 50));
   return (
     <>
-    <div style={{position:"relative"}}>
-
-    <Navbar />
-    
+      <div style={{ position: "relative",overflow:"hidden" }}>
+        <Navbar />
+        <Grid />
         <div className="ball2"></div>
-    <div style={{overflow:"hidden",position:"relative",height:"100vh" }}>
-        <Spline style={{position:"absolute"}} scene="https://prod.spline.design/GzVVx11nnOZkogxx/scene.splinecode" />
-        <div className="ball1"></div>
         <div
-          style={{
-            width: "100vw",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            paddingTop: "110px",
-            gap: "10px",
-            position: "relative",
-            // overflowY: "hidden",
-            // height: "100vh",
-          }}
+          style={{ overflow: "hidden", position: "relative", height: "100vh",display:"flex",flexDirection:"column",justifyContent:"space-evenly",alignItems:"center" }}
         >
-          <span className="iitkHeading" >
-            IIT Kanpur's{" "}
-          </span>
-          <span style={{display:"block",width:"1vw",height:"10px"}}></span>
-          <span
-            className="startupsHeading"
-          >
-            {" "}Startups
-          </span>
-        </div>
-        <div
-          style={{
-            // fontWeight: "400",
-            fontFamily: "Poppins",
-            fontSize: "calc(34px + 4.3 * ((100vw - 320px) / 435))",
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          <span
+          <Spline
+            style={{ position: "absolute" }}
+            scene="https://prod.spline.design/GzVVx11nnOZkogxx/scene.splinecode"
+          />
+          <div className="ball1"></div>
+          <div
             style={{
-              backgroundColor: "black",
-              color: "white",
-              borderRadius: "10px",
+              width: "100vw",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              paddingTop: "110px",
+              gap: "10px",
+              position: "relative",
+              // overflowY: "hidden",
+              // height: "100vh",
+            }}
+            className="iitkStartupsHeading"
+          >
+            <span className="iitkHeading">IIT Kanpur's </span>
+            <span
+              
+              className="iitkHeadingGap"
+            ></span>
+            <span className="startupsHeading"> Startups</span>
+          </div>
+          <div
+            style={{
+              // fontWeight: "400",
+              fontFamily: "Poppins",
+              fontSize: "calc(29px + 5 * ((100vw - 320px) / 535))",
+              display: "flex",
+              justifyContent: "center",
             }}
           >
+            <span
+              style={{
+                backgroundColor: "black",
+                color: "white",
+                borderRadius: "10px",
+              }}
+            >
+              {" "}
+              GENESIS{" "}
+            </span>
+          </div>
+          <div
+            style={{
+              textAlign: "center",
+              fontWeight: "500",
+              fontSize: "calc(10px + 4.3 * ((100vw - 320px) /535))",
+              padding: "25px",
+              color: "wheat",
+            }}
+          >
+            AT IIT KANPUR, INNOVATION MEETS AMBITION, CREATING AN
+            ENTREPRENEURIAL EPICENTER. OUR VIBRANT ECOSYSTEM BIRTHS STARTUPS
+            THAT REDEFINE THE FUTURE
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "20px",
+              // height: "100px",
+              // width: "100vw",
+            }}
+          >
+            <img
+              style={{ filter: "invert(100%)", cursor: "pointer" }}
+              src="./iitk-startups-23/images/arrow.png"
+              alt=""
+              srcset=""
+              width="30px"
+              // height="30px"
+            />
+          </div>
+        </div>
+        <div
+          style={{ maxWidth: "100vw", position: "relative", height: "100vh" }}
+        >
+          <div className="datacircle datacircle1">
+            <div></div>
+            <div></div>
+            <div className="text-in-data-circle">1300+ Startups</div>
+          </div>
+          <div className="datacircle datacircle2">
+            <div></div>
+            <div></div>
+            <div className="text-in-data-circle">1300+ Startups</div>
+          </div>
+          <div className="datacircle datacircle3">
+            <div></div>
+            <div></div>
+            <div className="text-in-data-circle">1300+ Startups</div>
+          </div>
+          
+          <Spline 
+            style={{ position: "absolute" }}
+            scene="https://prod.spline.design/v1cImhFNdRf5HDZg/scene.splinecode"
+          />
+        </div>
+        <div className="before-startups-data">
+          STARTUPS FLOURISH THROUGH THE IIT KANPUR ECOSYSTEM
+        </div>
+        <div
+            style={{
+              // margin: "20px",
+              width: "100%",
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "wrap",
+              justifyContent: "center",
+            }}
+          >
+            {result.map((item, index) => {
+              return (
+                <a
+                  key={index}
+                  style={{
+                    width: "100%",
+                    maxWidth: "220px",
 
-            {" "}
-            GENESIS{" "}
-          </span>
-        </div>
-        <div
-          style={{
-            textAlign: "center",
-            fontWeight: "500",
-            fontSize: "calc(14px + 4.3 * ((100vw - 320px) /535))",
-            padding: "25px",
-            color:"wheat",
-          }}
-        >
-          AT IIT KANPUR, INNOVATION MEETS AMBITION, CREATING AN ENTREPRENEURIAL
-          EPICENTER. OUR VIBRANT ECOSYSTEM BIRTHS STARTUPS THAT REDEFINE THE
-          FUTURE
-        </div>
-        <div
-          style={{ display: "flex", justifyContent: "center", padding: "20px" }}
-        >
-          <img src="./images/arrow.png" width="30px" />
-        </div>
+                    borderRadius: "10px",
+                    backgroundColor: "white",
+                    margin: "10px",
+                    padding: "10px",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    gap: "10px",
+                    textDecoration: "none",
+                    color: "black",
+                    boxShadow:
+                      "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset",
+                      zIndex:"4"
+                  }}
+                  href={item.Website_Link}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div style={{ width: "200px", height: "200px" }}>
+                    <img
+                      src={item.Image_Link}
+                      alt=""
+                      srcset=""
+                      // height="200px"
+                      width="200px"
+                      style={{ borderRadius: "10px" }}
+                    />
+                  </div>
+                  <div style={{ textAlign: "justify" }}>
+                    <div style={{ fontWeight: "500", fontSize: "24px" }}>
+                      {item.Name}
+                    </div>
+                    <div style={{ fontWeight: "400", fontSize: "15px" }}>
+                      {item.Short_Description}
+                    </div>
+                  </div>
+                </a>
+              );
+            })}
+          </div>
       </div>
-    <div style={{maxWidth:"100vw",position:"relative",height:"100vh"}}>
-      <Spline style={{position:"absolute"}} scene="https://prod.spline.design/v1cImhFNdRf5HDZg/scene.splinecode" />
-    </div>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, eaque velit quibusdam culpa debitis ducimus recusandae dolore sed cum praesentium totam nihil ex reiciendis pariatur facilis, obcaecati rem temporibus. Mollitia maiores aut similique possimus!
-    </div>
-    </div>
-
     </>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import "./app.css";
 
 const Navbar = () => {
   return (
-    <div style={{position:'fixed',top:"0",height:"55px",backgroundColor:"black",width:"100vw",overflow:"hidden"}}>
+    <div className='nav-glass' style={{position:'fixed',top:"0",height:"55px",width:"100vw",overflow:"hidden",zIndex:"2"}}>
       <div style={{height:"100%",display:"flex",justifyContent:"center",alignContent:"center",width:"fit-content"}}>
-        <img src="./images/logo.png" alt="" srcset="" height="50px"/>
+        <img src="./iitk-startups-23/images/logo.png" alt="E-Cell" srcset="" height="40px" />
       </div>
     </div>
   )
