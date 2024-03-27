1070b0dd1d13c89bfd94468184a4385059d4f902
Depanshu <depanshusahu057@gmail.com>
2024-03-27 21:19:45 +0530

    fitler option added

--- src/App.js
+++ src/App.js
@@ -4,19 +4,100 @@ import "./app.css";
 import Grid from "./Grid";
 import data from "./data.json";
 import ParticlesComponent from "./ParticlesComponent";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Paper from "@mui/material/Paper";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+
+const unicorn_status = [ "Any","Unicorn", "Soonicorn", "Minicorn"];
+const locations = ["Any","Noida", "Gurgaon", "Bengaluru", "Mumbai", "Palo Alto", "San Jose", "San Francisco",
+"Santa Clara", "San Mateo", "Thiruvananthapuram", "Gurugram", "Redwood City", "Pune",
+"Hyderabad", "Chennai", "Goleta", "Sunnyvale", "Claymont", "Delhi", "Thane", "Cambridge",
+"Kolkata", "Menlo Park", "Mountain View", "Bothell", "Singapore", "Ahmedabad", "Milpitas",
+"Bhopal", "Panaji", "Los Altos", "Boston", "Seattle", "Boulder", "Westlake Village",
+"New Castle", "Brossard", "Austin", "Cumming", "Kanpur", "Beaverton", "Plano", "Songpa-gu",
+"Ahmedabad", "Aliso Viejo", "Wilmington", "Newark", "Songpa-gu", "West Lafayette", "Walnut",
+"Redmond", "Reston", "Sunnyvale", "Toronto", "Madrid", "Riga", "St Lucia", "Princeton",
+"Karnal", "Sahibzada Ajit Singh Nagar", "Lucknow", "Chicago", "Indore", "Westerville", "Udaipur",
+"Ann Arbor"]
+
+const companyStages = [
+  "Any",
+  "Seed",
+  "Series A",
+  "Series B",
+  "Series C",
+  "Series D",
+  "Series E",
+  "Series F",
+  "Series H",
+  "Public",
+  "Acquired",
+  "Acqui-Hired",
+  "Unfunded",
+  "Deadpooled",
+  "Late Stage",
+  "Funding Raised"
+];
 
 function App() {
   const [result, setResult] = useState(data.slice(0, 50));
+  const [unicornStatus, setUnicornStatus] = useState("");
+  const [location,setLocation] = useState("");
+  const [companyStage,setCompanyStage] = useState("");
+
+  const handleData = () => { 
+
+    setResult(data.filter((item) => 
+      ((unicornStatus === "Any" || unicornStatus === "") ? 1 : item.Unicorn_Status.toLowerCase() === item.Unicorn_Status.toLowerCase())  &&
+      ((location === "Any" || location === "") ? 1 : item.Location.toLowerCase() === location.toLowerCase()) &&
+      ((companyStage === "Any" || companyStage === "") ? 1 : item.Company_Stage.toLowerCase() === companyStage.toLowerCase())
+    ))
+
+    // if ((unicornStatus === "Any" || unicornStatus==="") && (location === "Any" || location=== "") && (companyStage==="Any" || companyStage==="")) {
+    //   setResult(data.slice(0, 50));
+    // }else if(unicornStatus === "Any" || unicornStatus===""){
+    //   setResult(data.filter((item) => item.Unicorn_Status.toLowerCase() === unicornStatus.toLowerCase()));
+    // }else if(location === "Any" || location===""){
+    //   setResult(data.filter((item) => item.Location.toLowerCase() === location.toLowerCase()))
+    // }
+    //  else {
+    //   console.log(unicornStatus);
+    //   setResult(data.filter((item) => item.Unicorn_Status.toLowerCase() === unicornStatus.toLowerCase() && item.Location.toLowerCase() === location.toLowerCase()));
+    // }
+  }
+  useEffect(() => {
+    handleData();
+  }, [unicornStatus,location,companyStage]);
+  const handleChangeUnicornStatus = (event) => {
+    setUnicornStatus(event.target.value);
+  };
+  const handleChangeLocation = (event) => {
+    setLocation(event.target.value);
+  }
+  const handleChangeCompanyStage = (event) => {
+    setCompanyStage(event.target.value);
+  }
+
   return (
     <>
-      <div style={{ position: "relative",overflow:"hidden" }}>
+      <div style={{ position: "relative", overflow: "hidden" }}>
         <Navbar />
         <Grid />
         <ParticlesComponent />
         <div className="ball2"></div>
         <div
-          style={{ overflow: "hidden", position: "relative", height: "100vh",display:"flex",flexDirection:"column",justifyContent:"space-evenly",alignItems:"center" }}
+          style={{
+            overflow: "hidden",
+            position: "relative",
+            height: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
         >
           <Spline
             style={{ position: "absolute" }}
@@ -38,10 +119,7 @@ function App() {
             className="iitkStartupsHeading"
           >
             <span className="iitkHeading">IIT Kanpur's </span>
-            <span
-              
-              className="iitkHeadingGap"
-            ></span>
+            <span className="iitkHeadingGap"></span>
             <span className="startupsHeading"> Startups</span>
           </div>
           <div
@@ -114,8 +192,8 @@ function App() {
             <div></div>
             <div className="text-in-data-circle">1300+ Startups</div>
           </div>
-          
-          <Spline 
+
+          <Spline
             style={{ position: "absolute" }}
             scene="https://prod.spline.design/v1cImhFNdRf5HDZg/scene.splinecode"
           />
@@ -123,65 +201,132 @@ function App() {
         <div className="before-startups-data">
           STARTUPS FLOURISH THROUGH THE IIT KANPUR ECOSYSTEM
         </div>
-        <div
+        <div className="filter-box">
+          <Paper
+            elevation={3}
             style={{
-              // margin: "20px",
-              width: "100%",
-              display: "flex",
-              flexDirection: "row",
-              flexWrap: "wrap",
-              justifyContent: "center",
+              padding: "10px",
+              borderRadius: "10px",
+              maxWidth: "1200px",
+              margin: "auto",
+              backgroundColor:"whitesmoke"
             }}
           >
-            {result.map((item, index) => {
-              return (
-                <a
-                  key={index}
-                  style={{
-                    width: "100%",
-                    maxWidth: "220px",
+            <FormControl size="100">
+              <InputLabel id="demo-simple-select-label">
+                Unicorn Status
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={unicornStatus}
+                label="Unicorn Status"
+                onChange={handleChangeUnicornStatus}
+                style={{width:"200px"}}
+                // autoWidth
+              >
+                {unicorn_status.map((item, index) => {
+                  return <MenuItem value={item}>{item}</MenuItem>;
+                })}
+              </Select>
+            </FormControl>
+            <FormControl size="100">
+              <InputLabel id="demo-simple-select-label">
+                Location
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={location}
+                label="Location"
+                onChange={handleChangeLocation}
+                style={{width:"200px"}}
+                // autoWidth
+              >
+                {locations.map((item, index) => {
+                  return <MenuItem value={item}>{item}</MenuItem>;
+                })}
+              </Select>
+            </FormControl>
+            <FormControl size="100">
+              <InputLabel id="demo-simple-select-label">
+                Company Stage
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={companyStage}
+                label="Location"
+                onChange={handleChangeCompanyStage}
+                style={{width:"200px"}}
+                // autoWidth
+              >
+                {companyStages.map((item, index) => {
+                  return <MenuItem value={item}>{item}</MenuItem>;
+                })}
+              </Select>
+            </FormControl>
+          </Paper>
+        </div>
+        <div
+          style={{
+            // margin: "20px",
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "center",
+          }}
+        >
+          {result.map((item, index) => {
+            return (
+              <a
+                key={index}
+                style={{
+                  width: "100%",
+                  maxWidth: "220px",
 
-                    borderRadius: "10px",
-                    backgroundColor: "white",
-                    margin: "10px",
-                    padding: "10px",
-                    display: "flex",
-                    justifyContent: "space-between",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    gap: "10px",
-                    textDecoration: "none",
-                    color: "black",
-                    boxShadow:
-                      "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset",
-                      zIndex:"4"
-                  }}
-                  href={item.Website_Link}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <div style={{ width: "200px", height: "200px" }}>
-                    <img
-                      src={item.Image_Link}
-                      alt=""
-                      srcset=""
-                      // height="200px"
-                      width="200px"
-                      style={{ borderRadius: "10px" }}
-                    />
+                  borderRadius: "10px",
+                  backgroundColor: "white",
+                  margin: "10px",
+                  padding: "10px",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  gap: "10px",
+                  textDecoration: "none",
+                  color: "black",
+                  boxShadow:
+                    "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset",
+                  zIndex: "4",
+                }}
+                href={item.Website_Link}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div style={{ width: "200px", height: "200px" }}>
+                  <img
+                    src={item.Image_Link}
+                    alt=""
+                    srcset=""
+                    // height="200px"
+                    width="200px"
+                    style={{ borderRadius: "10px" }}
+                  />
+                </div>
+                <div style={{ textAlign: "justify" }}>
+                  <div style={{ fontWeight: "500", fontSize: "24px" }}>
+                    {item.Name}
                   </div>
-                  <div style={{ textAlign: "justify" }}>
-                    <div style={{ fontWeight: "500", fontSize: "24px" }}>
-                      {item.Name}
-                    </div>
-                    <div style={{ fontWeight: "400", fontSize: "15px" }}>
-                      {item.Short_Description}
-                    </div>
+                  <div style={{ fontWeight: "400", fontSize: "15px" }}>
+                    {item.Short_Description}
                   </div>
-                </a>
-              );
-            })}
-          </div>
+                </div>
+              </a>
+            );
+          })}
+        </div>
       </div>
     </>
   );
--- src/ParticlesComponent.js
+++ src/ParticlesComponent.js
@@ -4,7 +4,7 @@ import Particles from "react-tsparticles";
 import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
 const ParticlesComponent = () => { 
     const particlesInit = useCallback(async engine => {
-        console.log(engine);
+        // console.log(engine);
         // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
         // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
         // starting from v2 you can add only the features you need reducing the bundle size
